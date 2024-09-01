cccd15afd63e410b3aea089bd80e3f4ee838d0b7
sadeq <m.s.khodadadei@gmail.com>
2024-09-01 16:51:34 +0330

    ADD radial bar chart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:syncfusion_flutter_gauges/gauges.dart';
 
 void main() {
   runApp(const MyApp());
@@ -40,12 +43,12 @@ class _MyHomePageState extends State<MyHomePage> {
     if (request + user + 48 > 475) {
       double t = request + user + 48;
       t = t - 475;
-      print('t $t');
+      // print('t $t');
       t = t / 2;
       request = request - t;
       user = user - t;
     }
-    print('size bug   $bug' + 'size user   $user' + 'size request   $request');
+    // print('size bug   $bug' + 'size user   $user' + 'size request   $request');
 
     return Scaffold(
         body: Padding(
@@ -806,10 +809,280 @@ class _MyHomePageState extends State<MyHomePage> {
               padding: const EdgeInsets.fromLTRB(10, 74, 553, 0),
               child: Image.asset('images/PERSON.png'),
             ),
-          ])
+          ]),
 
 //navar status app part 2 of 3
 // finish
+          SizedBox(
+            height: 40,
+          ),
+          Row(
+            children: [
+              Container(
+                width: 430,
+                height: 280,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(25),
+                  color: Color(0xff232B2B),
+                ),
+                child: Column(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.only(top: 10),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            ' تعداد کاربرانی که در 24 ساعت گذشته از اپ دیدن کردن  ',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(bottom: 5),
+                            child: Point(colorpoint: Color(0xff5FE180)),
+                          ),
+                          SizedBox(
+                            width: 20,
+                          ),
+                        ],
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.fromLTRB(0, 5, 20, 0),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Text(
+                            'تعداد کاربرانی که در 24 ساعته گذشته به ما پیوستند  ',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(bottom: 5),
+                            child: Point(colorpoint: Color(0xff1F8AFF)),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                            width: 160,
+                            height: 150,
+                            child: SfRadialGauge(
+                              axes: <RadialAxis>[
+                                RadialAxis(
+                                  radiusFactor: 0.9,
+                                  pointers: const <GaugePointer>[
+                                    RangePointer(
+                                      width: 5,
+                                      value: 91,
+                                      cornerStyle: CornerStyle.bothFlat,
+                                      color: Color(0xff5FE180),
+                                    )
+                                  ],
+                                  interval: 5,
+                                  startAngle: 180,
+                                  endAngle: 0,
+                                  showTicks: false,
+                                  showLabels: false,
+                                  annotations: const <GaugeAnnotation>[
+                                    GaugeAnnotation(
+                                      widget: Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          SizedBox(
+                                            height: 40,
+                                          ),
+                                          Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.center,
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.center,
+                                            children: [
+                                              Point(
+                                                  colorpoint:
+                                                      Color(0xff5FE180)),
+                                              Text(
+                                                '918',
+                                                style: TextStyle(
+                                                    color: Colors.white,
+                                                    fontWeight: FontWeight.bold,
+                                                    fontSize: 12),
+                                              ),
+                                            ],
+                                          ),
+                                          SizedBox(
+                                            height: 5,
+                                          ),
+                                          Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.center,
+                                            children: [
+                                              Point(
+                                                  colorpoint:
+                                                      Color(0xff1F8AFF)),
+                                              Text(
+                                                '68',
+                                                style: TextStyle(
+                                                    color: Colors.white,
+                                                    fontWeight: FontWeight.bold,
+                                                    fontSize: 12),
+                                              ),
+                                            ],
+                                          )
+                                        ],
+                                      ),
+                                      angle: 270,
+                                      positionFactor: 0.2,
+                                    )
+                                  ],
+                                ),
+                                RadialAxis(
+                                  radiusFactor: 0.7,
+                                  pointers: [
+                                    RangePointer(
+                                      width: 5,
+                                      value: 68,
+                                      cornerStyle: CornerStyle.bothFlat,
+                                      color: Color(0xff1F8AFF),
+                                    )
+                                  ],
+                                  interval: 5,
+                                  startAngle: 180,
+                                  endAngle: 0,
+                                  showTicks: false,
+                                  showLabels: false,
+                                )
+                              ],
+                            ),
+                          ),
+                          const Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text(
+                                '10% پیشرفت ',
+                                style: TextStyle(color: Color(0xff17AC4D)),
+                              ),
+                              Text(
+                                '10% افول  ',
+                                style: TextStyle(
+                                    color: Color(0xffAC1719), fontSize: 15),
+                              ),
+                            ],
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(8, 0, 0, 0),
+                            child: Container(
+                              width: 160,
+                              height: 150,
+                              child: SfRadialGauge(
+                                axes: <RadialAxis>[
+                                  RadialAxis(
+                                    radiusFactor: 0.9,
+                                    pointers: const <GaugePointer>[
+                                      RangePointer(
+                                        value: 96,
+                                        cornerStyle: CornerStyle.bothFlat,
+                                        color: Color(0xff5FE180),
+                                        width: 5,
+                                      )
+                                    ],
+                                    interval: 5,
+                                    startAngle: 180,
+                                    endAngle: 0,
+                                    showTicks: false,
+                                    showLabels: false,
+                                    annotations: const <GaugeAnnotation>[
+                                      GaugeAnnotation(
+                                        widget: Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: [
+                                            SizedBox(
+                                              height: 40,
+                                            ),
+                                            Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.center,
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.center,
+                                              children: [
+                                                Point(
+                                                    colorpoint:
+                                                        Color(0xff5FE180)),
+                                                Text(
+                                                  '968',
+                                                  style: TextStyle(
+                                                      color: Colors.white,
+                                                      fontWeight:
+                                                          FontWeight.bold,
+                                                      fontSize: 12),
+                                                ),
+                                              ],
+                                            ),
+                                            SizedBox(
+                                              height: 5,
+                                            ),
+                                            Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.center,
+                                              children: [
+                                                Point(
+                                                    colorpoint:
+                                                        Color(0xff1F8AFF)),
+                                                Text(
+                                                  '58',
+                                                  style: TextStyle(
+                                                      color: Colors.white,
+                                                      fontWeight:
+                                                          FontWeight.bold,
+                                                      fontSize: 12),
+                                                ),
+                                              ],
+                                            )
+                                          ],
+                                        ),
+                                        angle: 270,
+                                        positionFactor: 0.2,
+                                      )
+                                    ],
+                                  ),
+                                  RadialAxis(
+                                    radiusFactor: 0.7,
+                                    pointers: [
+                                      RangePointer(
+                                        width: 5,
+                                        value: 58,
+                                        cornerStyle: CornerStyle.bothFlat,
+                                        color: Color(0xff1F8AFF),
+                                      )
+                                    ],
+                                    interval: 5,
+                                    startAngle: 180,
+                                    endAngle: 0,
+                                    showTicks: false,
+                                    showLabels: false,
+                                  )
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          )
         ],
       ),
     ));
